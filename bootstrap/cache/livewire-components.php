3cefa7fc202d7e23eb34a4fb057f5eafbb218e96
alaahahi <alaa.hahe@gmail.com>
2022-08-02 00:09:07 +0300

    Revert "Revert "fix""
    This reverts commit 708f8f40cd7c4929889a777c405e5905451cf23a.

---
+++ bootstrap/cache/livewire-components.php
@@ -0,0 +1,10 @@
+<?php return array (
+  'admin.admin-sale-component' => 'App\\Http\\Livewire\\Admin\\AdminSaleComponent',
+  'admin.search-bar' => 'App\\Http\\Livewire\\Admin\\SearchBar',
+  'attribute' => 'App\\Http\\Livewire\\Attribute',
+  'footer-detail' => 'App\\Http\\Livewire\\FooterDetail',
+  'nav-bar' => 'App\\Http\\Livewire\\NavBar',
+  'search-dropdown' => 'App\\Http\\Livewire\\SearchDropdown',
+  'show-posts' => 'App\\Http\\Livewire\\ShowPosts',
+  'show-product' => 'App\\Http\\Livewire\\ShowProduct',
+);
